78e9b32234a2a2d9555d3339aef4f0892f4efc69
Fabio Amorelli <amorelli.ff@gmail.com>
2022-11-28 16:31:09 -0300

    add docs and update readme.md

---
+++ docs.md
@@ -0,0 +1,236 @@
+# Documentação
+
+- Adicionar o autômato dos relacionais e das operações aritméticas no trabalho teórico;
+- Adicionar um quadrado da palavra reservada “for” na árvore de derivação;
+- Para o trabalho prático, foi preciso gerar os tokens literais, como o LIT_CHAR, LIT_INT e o LIT_FLOAT, assim como os tokens unitários de operadores lógicos, aritméticas e relacionais;
+- Não foi possível gerar um comando de saída que intercalasse strings com variáveis na mesma expressão; (Ainda dá tempo de implementar)
+- Variáveis constantes precisam ter valor atribuído quando forem declarados, pois ao traduzir para linguagem C, esta requer atribuição obrigatória;
+- As regras de produção mudaram significativamente, em algumas partes.
+- Foi necessário adicionar mais regras curtas, pois o analisador sintático, não conseguia acessar variáveis que ainda precisavam passar por mais uma produção.
+
+## Novas produções:
+
+##### Símbolo inicial
+
+```
+programa : MAIN BLOCK_START exprs BLOCK_END
+```
+
+##### Expressão vazia
+
+```
+exprs :
+```
+
+##### Expressão única sem ponto e vírgula
+
+```
+exprs : expr
+```
+
+##### Expressão única com ponto e vírgula
+
+```
+exprs : expr SEMICOLON
+```
+
+##### Expressões múltiplas sem ponto e vírgula
+
+```
+exprs : exprs expr
+```
+
+##### Expressões múltiplas com ponto e vírgula
+
+```
+exprs : exprs expr SEMICOLON
+```
+
+##### Expressão para output de string comum
+
+```
+expr : OUTPUT PAR_START ASP VAR ASP PAR_END
+```
+
+##### Expressão para output de variável
+
+```
+expr : OUTPUT PAR_START VAR PAR_END
+```
+
+##### Expressão para input de variável
+
+```
+expr : INPUT PAR_START VAR PAR_END
+```
+
+##### Expressão para declaração de inteiro
+
+```
+expr : LET VAR COLON INT
+```
+
+##### Expressão para atribuição de inteiro
+
+```
+expr :  VAR ATTR LIT_INT
+```
+
+##### Expressão para declaração de int com atribuição
+
+```
+expr : LET VAR COLON INT ATTR LIT_INT
+```
+
+##### Expressão para declaração de constante com atribuição obrigatória
+
+```
+expr : CONST VAR COLON INT ATTR LIT_INT
+```
+
+A mesma sequência de produções para inteiros foi criada para float e char como mostrado a seguir:
+
+##### Para float
+
+```
+expr : LET VAR COLON FLOAT
+```
+
+```
+expr :  VAR ATTR LIT_FLOAT
+```
+
+```
+expr : LET VAR COLON FLOAT ATTR LIT_FLOAT
+```
+
+```
+expr : CONST VAR COLON FLOAT ATTR LIT_FLOAT
+```
+
+##### Para char
+
+```
+expr : LET VAR COLON CHAR
+```
+
+```
+expr :  VAR ATTR LIT_CHAR
+```
+
+```
+expr : LET VAR COLON CHAR ATTR LIT_CHAR
+```
+
+```
+expr : CONST VAR COLON CHAR ATTR LIT_CHAR
+```
+
+##### Expressão para atribuição geral
+
+```
+expr : VAR ATTR expr
+```
+
+##### Expressão para operadores aritméticos
+
+```
+expr : expr SUM expr
+     | expr SUB expr
+     | expr MULT expr
+     | expr DIV expr
+     | expr MOD expr
+```
+
+##### Expressão para operadores relacionais
+
+```
+expr : expr EQUALS expr
+     | expr NOT_EQUALS expr
+     | expr GREATER expr
+     | expr SMALLER expr
+     | expr GREATER_EQUALS expr
+     | expr SMALLER_EQUALS expr
+```
+
+##### Expressão para operadores lógicos
+
+```
+expr : expr AND expr
+     | expr OR expr
+     | NOT expr
+```
+
+##### Expressão de condicional com único if
+
+```
+expr : IF PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END
+```
+
+##### Expressão de condicional com if-else
+
+```
+expr : IF PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END ELSE BLOCK_START exprs BLOCK_END
+```
+
+##### Expressão de loop while
+
+```
+expr : WHILE PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END
+```
+
+Para o loop for, foi necessário criar expressões auxiliares, denominadas como `exprfor`, pois a precedência de expressão e ponto e vírgula muda dentro do parênteses. Seguem as expressões criadas:
+
+##### Expressão única sem ponto e virgula
+
+```
+exprsfor : expr
+```
+
+##### Expressão múltipla que adiciona a nova expr à esquerda do ;
+
+```
+exprsfor : expr SEMICOLON exprsfor
+```
+
+##### Expressão para loop for
+
+```
+expr : FOR PAR_START exprsfor PAR_END BLOCK_START exprs BLOCK_END
+```
+
+##### Expressão que torna termo
+
+```
+expr : term
+```
+
+##### Termo que torna variável
+
+```
+term : VAR
+```
+
+##### Termo que torna int literal
+
+```
+term : LIT_INT
+```
+
+##### Termo que torna float literal
+
+```
+term : LIT_FLOAT
+```
+
+##### Termo que torna char literal
+
+```
+term : LIT_CHAR
+```
+
+##### Termo para adição de parênteses
+
+```
+term : PAR_START expr PAR_END
+```
--- src/must_yacc.py
+++ src/must_yacc.py
@@ -10,13 +10,14 @@ from must_lex import tokens
 myVariables = []
 # boolStack = []
 
+
 def hasInArray(name):
     index = 0
     for v in myVariables:
         if v['name'] == name:
             return index
         index += 1
-    
+
     return -1
 
 
@@ -27,11 +28,13 @@ precedence = (
     ('right', 'NOT')
 )
 
+
 def p_main(p):
     'programa : MAIN BLOCK_START exprs BLOCK_END'
     f = open("codigo_gerado.c", "w")
     f.write(f"#include <stdio.h>\nint main(){{\n   {p[3]}\n   return 0;\n}}")
     f.close()
+
 
 def p_exprs_void(p):
     '''
@@ -39,72 +42,81 @@ def p_exprs_void(p):
     '''
     p[0] = ""
 
+
 def p_exprs_no_semicolon(p):
     '''
     exprs : expr 
-    '''   
+    '''
     p[0] = p[1]
-    
+
 
 def p_exprs_single(p):
     '''
         exprs : expr SEMICOLON 
-    '''   
+    '''
     p[0] = p[1] + f";\n   "
+
 
 def p_expr_many_no_semicolon(p):
     '''
         exprs : exprs expr  
-    '''   
+    '''
     p[0] = p[1] + p[2]
+
 
 def p_expr_many(p):
     '''
         exprs : exprs expr SEMICOLON 
-    '''   
+    '''
     p[0] = p[1] + p[2] + f";\n   "
+
 
 def p_output_string(p):
     '''
     expr : OUTPUT PAR_START ASP VAR ASP PAR_END
-    '''       
+    '''
     if len(p) == 7:
         p[0] = f'printf("{p[4]}\\n")'
+
 
 def p_output_var(p):
     '''
     expr : OUTPUT PAR_START VAR PAR_END
-    '''   
-    if(hasInArray(p[3]) != -1):
-        if(myVariables[hasInArray(p[3])]['type'] == 'int'):
+    '''
+    if (hasInArray(p[3]) != -1):
+        if (myVariables[hasInArray(p[3])]['type'] == 'int'):
             p[0] = f'printf("%d\\n",{p[3]})'
-        elif(myVariables[hasInArray(p[3])]['type'] == 'float'):
+        elif (myVariables[hasInArray(p[3])]['type'] == 'float'):
             p[0] = f'printf("%f\\n",{p[3]})'
-        if(myVariables[hasInArray(p[3])]['type'] == 'char'):
+        if (myVariables[hasInArray(p[3])]['type'] == 'char'):
             p[0] = f'printf("%c\\n",{p[3]})'
+
 
 def p_input_var(p):
     '''
     expr : INPUT PAR_START VAR PAR_END
     '''
-    if(hasInArray(p[3]) != -1):
-        if(myVariables[hasInArray(p[3])]['type'] == 'int'):
+    if (hasInArray(p[3]) != -1):
+        if (myVariables[hasInArray(p[3])]['type'] == 'int'):
             p[0] = f'printf("Digite um valor para {p[3]}: ");\n   scanf("%d", &{p[3]})'
-        elif(myVariables[hasInArray(p[3])]['type'] == 'float'):
+        elif (myVariables[hasInArray(p[3])]['type'] == 'float'):
             p[0] = f'printf("Digite um valor para {p[3]}: ");\n   scanf("%f", &{p[3]})'
-        if(myVariables[hasInArray(p[3])]['type'] == 'char'):
+        if (myVariables[hasInArray(p[3])]['type'] == 'char'):
             p[0] = f'printf("Digite um valor para {p[3]}: ");\n   scanf("%c", &{p[3]})'
 
-#FUNCOES PARA INT
-#FUNCOES PARA INT
-#FUNCOES PARA INT
+# FUNCOES PARA INT
+# FUNCOES PARA INT
+# FUNCOES PARA INT
+
 
 def p_decl_int(p):
     '''
     expr : LET VAR COLON INT
     '''
-    myVariables.append({'name': p[2], 'type': 'int' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'int', 'value': None, 'mutabilty': 'let'})
     p[0] = f'int {p[2]}'
+
 
 def p_attr_int(p):
     '''
@@ -115,11 +127,13 @@ def p_attr_int(p):
 
     p[0] = f'{p[1]} = {p[3]}'
 
+
 def p_decl_let_int_attr(p):
     '''
     expr : LET VAR COLON INT ATTR LIT_INT
     '''
-    myVariables.append({'name': p[2], 'type': 'int' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'int', 'value': None, 'mutabilty': 'let'})
     p[0] = f'int {p[2]} = {p[6]}'
 
 
@@ -127,19 +141,23 @@ def p_decl_const_int_attr(p):
     '''
     expr : CONST VAR COLON INT ATTR LIT_INT
     '''
-    myVariables.append({'name': p[2], 'type': 'int' , 'value': None, 'mutabilty': 'const'})
+    myVariables.append(
+        {'name': p[2], 'type': 'int', 'value': None, 'mutabilty': 'const'})
     p[0] = f'const int {p[2]} = {p[6]}'
 
-#FUNCOES PARA FLOAT
-#FUNCOES PARA FLOAT
-#FUNCOES PARA FLOAT
+# FUNCOES PARA FLOAT
+# FUNCOES PARA FLOAT
+# FUNCOES PARA FLOAT
+
 
 def p_decl_float(p):
     '''
     expr : LET VAR COLON FLOAT
     '''
-    myVariables.append({'name': p[2], 'type': 'float' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'float', 'value': None, 'mutabilty': 'let'})
     p[0] = f'float {p[2]}'
+
 
 def p_attr_float(p):
     '''
@@ -150,11 +168,13 @@ def p_attr_float(p):
 
     p[0] = f'{p[1]} = {p[3]}'
 
+
 def p_decl_let_float_attr(p):
     '''
     expr : LET VAR COLON FLOAT ATTR LIT_FLOAT
     '''
-    myVariables.append({'name': p[2], 'type': 'float' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'float', 'value': None, 'mutabilty': 'let'})
     p[0] = f'float {p[2]} = {p[6]}'
 
 
@@ -162,20 +182,23 @@ def p_decl_const_float_attr(p):
     '''
     expr : CONST VAR COLON FLOAT ATTR LIT_FLOAT
     '''
-    myVariables.append({'name': p[2], 'type': 'float' , 'value': None, 'mutabilty': 'const'})
+    myVariables.append(
+        {'name': p[2], 'type': 'float', 'value': None, 'mutabilty': 'const'})
     p[0] = f'const float {p[2]} = {p[6]}'
 
 
-#FUNCOES PARA CHAR
-#FUNCOES PARA CHAR
-#FUNCOES PARA CHAR
+# FUNCOES PARA CHAR
+# FUNCOES PARA CHAR
+# FUNCOES PARA CHAR
 
 def p_decl_char(p):
     '''
     expr : LET VAR COLON CHAR
     '''
-    myVariables.append({'name': p[2], 'type': 'char' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'char', 'value': None, 'mutabilty': 'let'})
     p[0] = f'char {p[2]}'
+
 
 def p_attr_char(p):
     '''
@@ -186,11 +209,13 @@ def p_attr_char(p):
 
     p[0] = f'{p[1]} = {p[3]}'
 
+
 def p_decl_let_char_attr(p):
     '''
     expr : LET VAR COLON CHAR ATTR LIT_CHAR
     '''
-    myVariables.append({'name': p[2], 'type': 'char' , 'value': None, 'mutabilty': 'let'})
+    myVariables.append(
+        {'name': p[2], 'type': 'char', 'value': None, 'mutabilty': 'let'})
     p[0] = f'char {p[2]} = {p[6]}'
 
 
@@ -198,8 +223,10 @@ def p_decl_const_char_attr(p):
     '''
     expr : CONST VAR COLON CHAR ATTR LIT_CHAR
     '''
-    myVariables.append({'name': p[2], 'type': 'char' , 'value': None, 'mutabilty': 'const'})
+    myVariables.append(
+        {'name': p[2], 'type': 'char', 'value': None, 'mutabilty': 'const'})
     p[0] = f'const char {p[2]} = {p[6]}'
+
 
 def p_attr(p):
     '''
@@ -209,6 +236,7 @@ def p_attr(p):
         myVariables[hasInArray(p[1])]['value'] = p[3]
     p[0] = f'{p[1]} = {p[3]}'
 
+
 def p_expr_operations(p):
     '''
     expr : expr SUM expr
@@ -217,18 +245,19 @@ def p_expr_operations(p):
          | expr DIV expr
          | expr MOD expr
     '''
-    
+
     match p[2]:
         case '+':
-            p[0] = f"{p[1]} + {p[3]}" 
+            p[0] = f"{p[1]} + {p[3]}"
         case '-':
-            p[0] = f"{p[1]} - {p[3]}" 
+            p[0] = f"{p[1]} - {p[3]}"
         case '*':
-            p[0] = f"{p[1]} * {p[3]}" 
+            p[0] = f"{p[1]} * {p[3]}"
         case '/':
-            p[0] = f"{p[1]} / {p[3]}" 
+            p[0] = f"{p[1]} / {p[3]}"
         case '%':
-            p[0] = f"{p[1]} % {p[3]}" 
+            p[0] = f"{p[1]} % {p[3]}"
+
 
 def p_expr_relationals(p):
     '''
@@ -270,46 +299,53 @@ def p_expr_logicals(p):
         case '||':
             p[0] = f"{p[1]} || {p[3]}"
 
+
 def p_cond_if_only(p):
     '''
     expr : IF PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END
-    '''      
+    '''
     p[0] = f"if({p[3]}){{ \n {p[6]} \n }} "
+
 
 def p_cond_if_else(p):
     '''
     expr : IF PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END ELSE BLOCK_START exprs BLOCK_END
-    '''         
+    '''
     p[0] = f"if({p[3]}){{ \n {p[6]} \n }} else {{ \n {p[10]} }}"
 
-def p_while(p):    
+
+def p_while(p):
     '''
     expr : WHILE PAR_START exprs PAR_END BLOCK_START exprs BLOCK_END
-    ''' 
+    '''
     p[0] = f"while({p[3]}){{ \n {p[6]} \n }}"
+
 
 def p_exprs_no_semicolon(p):
     '''
     exprsfor : expr 
-    '''   
+    '''
     p[0] = p[1]
+
 
 def p_exprs_for_no_breakline(p):
     '''
         exprsfor :  expr SEMICOLON exprsfor
-    '''   
-    p[0] = p[1] + f"; " + p[3] 
+    '''
+    p[0] = p[1] + f"; " + p[3]
 
 
-def p_for(p):    
+def p_for(p):
     '''
     expr : FOR PAR_START exprsfor PAR_END BLOCK_START exprs BLOCK_END
-    ''' 
+    '''
     p[0] = f"for({p[3]}){{ \n {p[6]}  }}"
+
 
 def p_expr_term(p):
     'expr : term'
     p[0] = p[1]
+
 
 def p_term_literal_char(p):
     'term : LIT_CHAR'
@@ -319,6 +355,7 @@ def p_term_literal_char(p):
 def p_term_literal_var(p):
     'term : VAR'
     p[0] = p[1]
+
 
 def p_term_literal_int(p):
     'term : LIT_INT'
@@ -334,16 +371,16 @@ def p_term_par_expr(p):
     'term : PAR_START expr PAR_END'
     p[0] = f'({p[2]})'
 
-def p_error(t):
-    if t is not None :
-        print("Syntax error at '%s'" % t.value)
 
+def p_error(t):
+    if t is not None:
+        print("Syntax error at '%s'" % t.value)
 
 
 parser = yacc.yacc()
 
 print('\n----- fim codigo -----\n')
-#entrada de teste para output de string comum
+# entrada de teste para output de string comum
 data0 = '''
     main {
         output("Hello");
@@ -351,7 +388,7 @@ data0 = '''
     }
 '''
 
-#entrada de teste para decl attr e output de variavel inteiras
+# entrada de teste para decl attr e output de variavel inteiras
 data1 = '''
 main {
     let variavel_int: int;
@@ -364,7 +401,7 @@ main {
 }
 '''
 
-#entrada de teste para decl attr e output de variavel float
+# entrada de teste para decl attr e output de variavel float
 data2 = '''
 main {
     let variavel_float: float;
@@ -378,7 +415,7 @@ main {
 }
 '''
 
-#entrada de teste para decl attr e output de variavel char
+# entrada de teste para decl attr e output de variavel char
 data3 = '''
 main {
     let variavel_char: char;
@@ -392,7 +429,7 @@ main {
 }
 '''
 
-#entrada de teste para attr com operacao aritmetica
+# entrada de teste para attr com operacao aritmetica
 data4 = '''
 main {
     let variavel_int: int;
@@ -404,7 +441,7 @@ main {
 }
 '''
 
-#entrada de teste para cond logical e relacional verdadeiro
+# entrada de teste para cond logical e relacional verdadeiro
 data5 = '''
 main {
     let variavel_int: int;
@@ -418,7 +455,7 @@ main {
 }
 '''
 
-#entrada de teste para cond logical e relacional falso com else
+# entrada de teste para cond logical e relacional falso com else
 data6 = '''
 main {
     let variavel_int: int;
@@ -434,7 +471,7 @@ main {
 }
 '''
 
-#entrada de teste para while
+# entrada de teste para while
 data7 = '''
 main {
     let variavel_int: int;
@@ -446,7 +483,7 @@ main {
 }
 '''
 
-#entrada de teste para for
+# entrada de teste para for
 data8 = '''
 main {
     let variavel_int: int;
@@ -461,253 +498,3 @@ main {
 print('ENTRADA: ', data8)
 
 result = parser.parse(data8)
-
-
-
-######################################  Codigo antigo ####################################
-# def p_decl(p):
-#     '''
-#     expr : LET VAR COLON INT
-#          | LET VAR COLON FLOAT
-#          | LET VAR COLON CHAR
-#          | LET VAR COLON INT ATTR LIT_INT
-#          | LET VAR COLON FLOAT ATTR LIT_FLOAT
-#          | LET VAR COLON CHAR ATTR LIT_CHAR
-#          | CONST VAR COLON INT
-#          | CONST VAR COLON FLOAT
-#          | CONST VAR COLON CHAR
-#          | CONST VAR COLON INT ATTR LIT_INT
-#          | CONST VAR COLON FLOAT ATTR LIT_FLOAT
-#          | CONST VAR COLON CHAR ATTR LIT_CHAR
-#     '''
-
-#     if len(p) == 5:
-#         print('let aqui')
-#     elif len(p) == 7:
-        
-#         if len(p[6]) == 3:
-#             pass
-#         else: 
-#             pass
-
-# def p_attr(p):
-#     '''
-#     expr :  VAR ATTR LIT_INT
-#           | VAR ATTR LIT_FLOAT
-#           | VAR ATTR LIT_CHAR
-#           | VAR ATTR expr  
-#     '''
-
-# def p_expr_operations(p):
-#     '''
-#     expr : expr SUM expr
-#          | expr SUB expr
-#          | expr MULT factor
-#          | expr DIV factor
-#          | expr MOD expr
-#     '''
-#     if(hasInArray(p[1]) != -1):
-#         p[1] = myVariables[hasInArray(p[1])]['value']
-        
-#     match p[2]:
-#         case '+':
-#             p[0] = p[1] + p[3]
-#         case '-':
-#             p[0] = p[1] - p[3]
-#         case '*':
-#             p[0] = p[1] * p[3]
-#         case '/':
-#             p[0] = p[1] / p[3]
-#         case '%':
-#             p[0] = p[1] % p[3]
-
-
-# def p_expr_relationals(p):
-#     '''
-#     expr : expr EQUALS expr
-#          | expr NOT_EQUALS expr
-#          | expr GREATER expr
-#          | expr SMALLER expr
-#          | expr GREATER_EQUALS expr
-#          | expr SMALLER_EQUALS expr
-#     '''
-#     if(hasInArray(p[1]) != -1):
-#         match myVariables[hasInArray(p[1])]['type']:
-#             case 'int':
-#                 p[1] = int(myVariables[hasInArray(p[1])]['value'])
-#             case 'float':
-#                 p[1] = float(myVariables[hasInArray(p[1])]['value'])
-#             case 'char':
-#                 p[1] = myVariables[hasInArray(p[1])]['value']
-#     match p[2]:
-#         case '==':
-#             p[0] = p[1] == p[3]
-#         case '!=':
-#             p[0] = p[1] != p[3]
-#         case '>':
-#             p[0] = p[1] > p[3]
-#         case '<':
-#             p[0] = p[1] < p[3]
-#         case '>=':
-#             p[0] = p[1] >= p[3]
-#         case '<=':
-#             p[0] = p[1] <= p[3]
-
-
-# def p_expr_logicals(p):
-#     '''
-#     expr : expr AND expr
-#          | expr OR expr
-#          | NOT expr
-#     '''
-
-#     if p[1] == '!':
-#         p[0] = not p[2]
-
-#     match p[2]:
-#         case '&&':
-#             p[0] = p[1] and p[3]
-#         case '||':
-#             p[0] = p[1] or p[3]
-
-
-# def p_decl(p):
-#     '''
-#     term : LET VAR COLON INT
-#          | LET VAR COLON FLOAT
-#          | LET VAR COLON CHAR
-#          | LET VAR COLON INT ATTR LIT_INT
-#          | LET VAR COLON FLOAT ATTR LIT_FLOAT
-#          | LET VAR COLON CHAR ATTR LIT_CHAR
-#          | CONST VAR COLON INT
-#          | CONST VAR COLON FLOAT
-#          | CONST VAR COLON CHAR
-#          | CONST VAR COLON INT ATTR LIT_INT
-#          | CONST VAR COLON FLOAT ATTR LIT_FLOAT
-#          | CONST VAR COLON CHAR ATTR LIT_CHAR
-#     '''
-
-#     if len(p) == 5:
-#         myVariables.append({'name': p[2], 'type': p[4] , 'value': None})
-#     elif len(p) == 7:
-#         if len(p[6]) == 3:
-#             myVariables.append({'name': p[2], 'type': p[4] , 'value': p[6][1]})
-#         else: 
-#             myVariables.append({'name': p[2], 'type': p[4] , 'value': p[6]})
-
-
-# def p_attr(p):
-#     '''
-#     term :  VAR ATTR LIT_INT
-#           | VAR ATTR LIT_FLOAT
-#           | VAR ATTR LIT_CHAR
-#           | VAR ATTR expr  
-#     '''
-#     if hasInArray(p[1]) != -1:
-#         if myVariables[hasInArray(p[1])]['type'] == 'char':
-#             myVariables[hasInArray(p[1])]['value'] = p[3][1]
-#         else: 
-#             myVariables[hasInArray(p[1])]['value'] = p[3]
-
-
-
-
-
-# def p_main(p):
-#     'term : MAIN BLOCK_START expr BLOCK_END'
-
-
-# def p_term_literal_char(p):
-#     'term : LIT_CHAR'
-#     p[0] = p[1][1]
-
-
-# def p_term_literal_var(p):
-#     'term : VAR'
-#     p[0] = p[1]
-
-
-# def p_term_factor(p):
-#     'term : factor'
-#     p[0] = p[1]
-
-
-# def p_term_literal_int(p):
-#     'factor : LIT_INT'
-#     p[0] = int(p[1])
-
-
-# def p_term_literal_float(p):
-#     'factor : LIT_FLOAT'
-#     p[0] = float(p[1])
-
-
-# def p_expr_factor(p):
-#     'factor : PAR_START expr PAR_END'
-#     p[0] = p[2]
-
-# def p_cond(p):
-#     '''
-#     term : IF PAR_START expr PAR_END BLOCK_START expr BLOCK_END
-#          | IF PAR_START expr PAR_END BLOCK_START expr BLOCK_END ELSE BLOCK_START expr BLOCK_END
-#     '''
-#     #falta adicionar o funcionamento
-#     if p[3]:
-#         boolStack.append(True)
-#     elif len(p) == 12:
-#         boolStack.append(False)
-#     else:
-#         boolStack.append(False)
-
-
-# def p_output(p):
-#     '''
-#     term : OUTPUT PAR_START expr PAR_END
-#          | OUTPUT PAR_START ASP expr ASP PAR_END
-#     '''       
-#     if len(p) <= 5:
-#         if hasInArray(p[3]) != -1:
-#             value = myVariables[hasInArray(p[3])]['value'];
-#             typeVar = myVariables[hasInArray(p[3])]['type'];
-#             if typeVar == 'int' or typeVar == 'float':
-#                 print(value)
-#             elif typeVar == 'char':
-#                 print(value)
-#         else:
-#             print(p[3])
-#     else:
-#         if len(p) ==7: 
-#             print(p[4])
-# def p_input(p):
-#     '''
-#     term : INPUT PAR_START VAR PAR_END
-#     '''
-#     if hasInArray(p[3]) != -1:
-#         typeVar = myVariables[hasInArray(p[3])]['type']
-#         inputValue = input("Digite o valor para " + myVariables[hasInArray(p[3])]['name'] +': ')  
-#         if typeVar == 'int' and  type(int(inputValue)) is int:
-#             myVariables[hasInArray(p[3])]['value'] =  int(inputValue) 
-#         elif typeVar == 'float' and  type(float(inputValue)) is float:
-#             myVariables[hasInArray(p[3])]['value'] =  float(inputValue)  
-#         elif typeVar == 'char' and len(inputValue) == 3:    
-#             myVariables[hasInArray(p[3])]['value'] = inputValue[1]
-#         else:
-#             print("Não foi possível atribuir, erro de tipagem")
-
-
-# def p_line(p):
-#     '''
-#     term : term SEMICOLON
-#     term : term SEMICOLON term
-#     term : term SEMICOLON BLOCK_END
-#     '''
-
-# def p_while(p):
-#     '''
-#     term : WHILE PAR_START expr PAR_END BLOCK_START term BLOCK_END
-#     '''        
-#     while p[3]:
-#         p[0] = p[6]
-
-##
-######################################  Codigo antigo ####################################
